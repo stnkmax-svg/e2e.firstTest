f93b64de238adb5bdde3245902892dfcfb42e63d
mkvczz <stnk.max@gmail.com>
2026-02-03 22:00:49 +0300

    corrected imports

--- tests/2e2.spec.js
+++ tests/2e2.spec.js
@@ -1,5 +1,5 @@
 const { test, expect } = require('@playwright/test');
-import { LoginPage } from '../pages/loginpage';
+import { LoginPage } from '../pages/LoginPage'
 import { InventoryPage } from '../pages/InventoryPage';
 import { CartPage } from '../pages/CartPage';
 import { CheckoutStepOnePage } from '../pages/CheckoutStepOnePage';
